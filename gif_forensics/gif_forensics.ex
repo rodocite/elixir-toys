d18465295416522f178fbb8dcd0caf33d2aa8786
Rodolfo Yabut <rodolfoyabut@gmail.com>
2017-06-15 05:54:13 -0700

    Try different form for case
    This form makes it immediately clear that the return from a function
    call is piped into the case block.

--- gif_forensics/gif_forensics.ex
+++ gif_forensics/gif_forensics.ex
@@ -1,6 +1,7 @@
 defmodule GifForensics do
   def version(gif_file) do
-    case File.read(gif_file) do
+    File.read(gif_file)
+    |> case do
       {:ok, gif} -> 
         << gif_header :: binary-size(6), _ :: binary >> = gif
         IO.puts(gif_header)
