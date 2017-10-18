8ec63336e0991b23bea2bb1ad8bd4d158c0f3594
Rodolfo Yabut <rodolfoyabut@gmail.com>
2017-10-18 05:47:08 -0700

    Elixir Blockchain Toy

--- blockchain/lib/blockchain.ex
+++ blockchain/lib/blockchain.ex
@@ -1,18 +1,53 @@
 defmodule Blockchain do
-  @moduledoc """
-  Documentation for Blockchain.
-  """
+  def start_link do
+    blockchain = %{
+      chain: [],
+      current_transactions: [],
+    }
+    Agent.start_link(fn -> blockchain end, name: __MODULE__)
+  end
 
-  @doc """
-  Hello world.
+  def new_block(previous_hash \\ nil) do
+    current_state = Agent.get(__MODULE__, fn state -> state end)
+    chain = current_state[:chain]
+    last_index = length(chain)
+    previous_block = Enum.fetch(chain, 0)
 
-  ## Examples
+    IO.inspect(current_state)
 
-      iex> Blockchain.hello
-      :world
+    block = %{
+        index: last_index + 1,
+        timestamp: Time.utc_now,
+        transactions: current_state.current_transactions,
+        previous_hash: previous_hash || hash(previous_block)
+    }
 
-  """
-  def hello do
-    :world
+    __MODULE__ |>
+    Agent.update(
+      fn blockchain ->
+        chain = blockchain[:chain]
+        %{blockchain | chain: [] ++ chain ++ [block]}
+      end
+    )
+  end
+
+  def new_transaction(record) do
+    record |>
+    case do
+      record ->
+        __MODULE__ |>
+        Agent.update(
+          fn blockchain ->
+            current_transactions = blockchain[:current_transactions]
+            %{blockchain | current_transactions: [] ++ current_transactions ++ [record]}
+          end
+        )
+    end
+  end
+
+  def hash({:ok, block}) do
+    Agent.get(__MODULE__, fn state -> IO.inspect(state)end)
+    :crypto.hmac(:sha256, "so secure, bro", Poison.encode!(block))
+    |> Base.encode16
   end
 end
